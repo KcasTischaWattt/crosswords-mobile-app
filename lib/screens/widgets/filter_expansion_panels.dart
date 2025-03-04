26ac9be7f670d7195568a3ede82b505218360db9
Matvey Tataurov <tmnbob2012@gmail.com>
2025-03-04 19:00:06 +0300

    Remove unused provider import in FilterExpansionPanels

--- lib/screens/widgets/filter_expansion_panels.dart
+++ lib/screens/widgets/filter_expansion_panels.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../../providers/abstract/filter_provider.dart';
 
 class FilterExpansionPanels extends StatefulWidget {
