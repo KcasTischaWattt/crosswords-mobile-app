a0244a8fdcc854fd24a68a4f36e32536a8c22f06
Matvey Tataurov <tmnbob2012@gmail.com>
2025-03-04 19:05:41 +0300

    Refactor FilterExpansionPanels

--- lib/screens/widgets/filter_expansion_panels.dart
+++ lib/screens/widgets/filter_expansion_panels.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import '../../providers/abstract/filter_provider.dart';
 
 class FilterExpansionPanels extends StatefulWidget {
-
   final FilterProvider provider;
 
   const FilterExpansionPanels({super.key, required this.provider});
@@ -12,94 +11,87 @@ class FilterExpansionPanels extends StatefulWidget {
 }
 
 class _FilterExpansionPanelsState extends State<FilterExpansionPanels> {
+  ExpansionPanelRadio _buildExpansionPanel({
+    required String value,
+    required String title,
+    required List<String> items,
+    required List<String> selectedItems,
+    required void Function(String) onToggle,
+  }) {
+    return ExpansionPanelRadio(
+      value: value,
+      canTapOnHeader: true,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      headerBuilder: (BuildContext context, bool isExpanded) {
+        return ListTile(
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
+      body: _buildCheckboxList(items, selectedItems, onToggle),
+    );
+  }
+
+  Widget _buildCheckboxList(List<String> items, List<String> selectedItems,
+      void Function(String) onToggle) {
+    return Container(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      constraints: const BoxConstraints(maxHeight: 140),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      child: SingleChildScrollView(
+        child: Column(
+          children: items.map((item) {
+            return _buildCheckboxItem(item, selectedItems, onToggle);
+          }).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCheckboxItem(
+      String item, List<String> selectedItems, void Function(String) onToggle) {
+    return CheckboxListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 6),
+      title: Text(item, style: const TextStyle(fontSize: 14)),
+      value: selectedItems.contains(item),
+      onChanged: (bool? value) {
+        setState(() {
+          onToggle(item);
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final provider = widget.provider;
-
     return Container(
       color: Theme.of(context).scaffoldBackgroundColor,
       child: ExpansionPanelList.radio(
         elevation: 0,
         expandedHeaderPadding: EdgeInsets.zero,
         children: [
-          ExpansionPanelRadio(
+          _buildExpansionPanel(
             value: 'sources',
-            canTapOnHeader: true,
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            headerBuilder: (BuildContext context, bool isExpanded) {
-              return ListTile(
-                title: Text(
-                  'Источники',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              );
-            },
-            body: Container(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              constraints: const BoxConstraints(maxHeight: 140),
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: provider.sources.map((item) {
-                    return CheckboxListTile(
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 6),
-                      title: Text(item, style: const TextStyle(fontSize: 14)),
-                      value: provider.selectedSources.contains(item),
-                      onChanged: (bool? value) {
-                        setState(() {
-                          provider.toggleSource(item);
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
+            title: 'Источники',
+            items: widget.provider.sources,
+            selectedItems: widget.provider.selectedSources,
+            onToggle: widget.provider.toggleSource,
           ),
-
-          // теги
-          ExpansionPanelRadio(
+          _buildExpansionPanel(
             value: 'tags',
-            canTapOnHeader: true,
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            headerBuilder: (BuildContext context, bool isExpanded) {
-              return ListTile(
-                title: Text(
-                  'Тэги',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              );
-            },
-            body: Container(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              constraints: const BoxConstraints(maxHeight: 140),
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: provider.tags.map((item) {
-                    return CheckboxListTile(
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 6),
-                      title: Text(item, style: const TextStyle(fontSize: 14)),
-                      value: provider.selectedTags.contains(item),
-                      onChanged: (bool? value) {
-                        setState(() {
-                          provider.toggleTag(item);
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
+            title: 'Тэги',
+            items: widget.provider.tags,
+            selectedItems: widget.provider.selectedTags,
+            onToggle: widget.provider.toggleTag,
           ),
         ],
       ),
     );
+
   }
 }
