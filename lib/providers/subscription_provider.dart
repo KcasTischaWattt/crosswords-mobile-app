dce4fa982ecce5a3b181aeb683b0825f494c3844
Matvey Tataurov <tmnbob2012@gmail.com>
2025-03-09 13:09:13 +0300

    Implement filter functionality in SubscriptionProvider with sources and tags

--- lib/providers/subscription_provider.dart
+++ lib/providers/subscription_provider.dart
@@ -1,17 +1,43 @@
+import 'package:crosswords/providers/abstract/filter_provider.dart';
 import 'package:flutter/material.dart';
 import '../data/models/subscription.dart';
 import '../data/fake/fake_subscriptions.dart';
 
-class SubscriptionProvider extends ChangeNotifier {
+class SubscriptionProvider extends ChangeNotifier implements FilterProvider {
   List<Subscription> _subscriptions = [];
+  List<String> _sources = [
+    'Источник 1',
+    'Источник 2',
+    'Источник 3',
+    'Источник 4',
+    'Источник 5',
+    'Источник 6'
+  ];
+  List<String> _tags = ['Тэг 1', 'Тэг 2', 'Тэг 3', 'Тэг 4', 'Тэг 5', 'Тэг 6'];
 
   int? _selectedSubscriptionId;
   bool _isLoading = false;
 
   bool get isLoading => _isLoading;
 
+  List<String> _selectedSources = [];
+  List<String> _selectedTags = [];
+
   int? get selectedSubscriptionId => _selectedSubscriptionId;
+
   List<Subscription> get subscriptions => _subscriptions;
+
+  @override
+  List<String> get selectedSources => _selectedSources;
+
+  @override
+  List<String> get selectedTags => _selectedTags;
+
+  @override
+  List<String> get sources => _sources;
+
+  @override
+  List<String> get tags => _tags;
 
   void setSelectedSubscription(int? subscriptionId) {
     _selectedSubscriptionId = subscriptionId;
@@ -58,5 +84,23 @@ class SubscriptionProvider extends ChangeNotifier {
 
     _isLoading = false;
     notifyListeners();
+  }
+
+  @override
+  void toggleSource(String source) {
+    if (_selectedSources.contains(source)) {
+      _selectedSources.remove(source);
+    } else {
+      _selectedSources.add(source);
+    }
+  }
+
+  @override
+  void toggleTag(String tag) {
+    if (_selectedTags.contains(tag)) {
+      _selectedTags.remove(tag);
+    } else {
+      _selectedTags.add(tag);
+    }
   }
 }
