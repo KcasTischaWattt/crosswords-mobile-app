8f8ae94c8675da8f24640a412a3c27d9906b245a
Matvey Tataurov <tmnbob2012@gmail.com>
2025-03-14 13:10:24 +0300

    Refactor NotificationsPage to use StatefulWidget and improve AppBar design

--- lib/screens/notifications_page.dart
+++ lib/screens/notifications_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
+  const NotificationsPage({super.key});
+
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      toolbarHeight: 60,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      surfaceTintColor: Colors.transparent,
+      elevation: 0,
+      shadowColor: Colors.transparent,
+      title: const Text(
+        'Уведомления',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Уведомления')),
+      appBar: _buildAppBar(),
       body: Center(child: Text('Ваши уведомления', style: TextStyle(fontSize: 18))),
     );
   }
