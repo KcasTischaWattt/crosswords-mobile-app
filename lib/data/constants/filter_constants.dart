ef7f6fa1a92ed6a2a4568904744d49e6ff886651
Matvey Tataurov <tmnbob2012@gmail.com>
2025-04-26 15:21:19 +0300

    Refactor tag constants to use sorted keys from tagIcons

--- lib/data/constants/filter_constants.dart
+++ lib/data/constants/filter_constants.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'package:crosswords/data/constants/tag_icons.dart';
 
 final List<String> _sources = [
   'Коммерсант',
@@ -10,18 +11,8 @@ final List<String> _sources = [
   'ЦБ Азербайджан',
 ];
 
-final List<String> _tags = [
-  'Политика',
-  'Экономика',
-  'Технологии',
-  'Спорт',
-  'Кредит',
-  'IT',
-  'Зарплаты',
-  'Кибербезопасность',
-  'Футбол',
-  'СБП',
-];
+final List<String> _tags = (tagIcons.keys.toList()..sort());
 
-final UnmodifiableListView<String> defaultSources = UnmodifiableListView(_sources);
+final UnmodifiableListView<String> defaultSources =
+    UnmodifiableListView(_sources);
 final UnmodifiableListView<String> defaultTags = UnmodifiableListView(_tags);
